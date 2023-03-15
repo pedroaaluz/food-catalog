d3c0ed85ef4a77963191c615162b9189fe7b382a
pedroaaluz <pedroaluz137@gmail.com>
2023-03-15 07:05:44 -0300

    refactor: :lipstick: new tab bar

--- src/route.tsx
+++ src/route.tsx
@@ -6,36 +6,43 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {RootStackParamList} from './types/rootStackParamListType';
-import Icon from 'react-native-vector-icons/Feather';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 import type {} from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator<RootStackParamList>();
 // RouteProp<RootStackParamList, keyof RootStackParamList>
 
+const iconsFocused = {
+  Catalog: 'book-open',
+  Favorites: 'star',
+};
+
+const iconsNoFocused = {
+  Catalog: 'book',
+  Favorites: 'star',
+};
+
 export const Routes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Catalog"
-          component={Catalog}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({color}) => (
-              <Icon name="book" size={30} color="#900" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Favorites"
-          component={Favorites}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({color}) => (
-              <Icon name="book" size={30} color="#900" />
-            ),
-          }}
-        />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            const iconName = focused
+              ? iconsFocused[route.name]
+              : iconsNoFocused[route.name];
+
+            // You can return any component that you like here!
+            return <FeatherIcon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#3f72af',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+          tabBarAllowFontScaling: true,
+          tabBarShowLabel: false,
+        })}>
+        <Tab.Screen name="Catalog" component={Catalog} />
+        <Tab.Screen name="Favorites" component={Favorites} />
       </Tab.Navigator>
     </NavigationContainer>
   );
