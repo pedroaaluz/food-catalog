0f79ee6a0f79a977d228a2e370bfebd9adabcd7d
pedroaaluz <pedroaluz137@gmail.com>
2023-03-20 20:08:44 -0300

    feat: :sparkles: add react query in catalog page

--- src/screens/catalog/index.tsx
+++ src/screens/catalog/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
   View,
   Text,
@@ -8,32 +8,15 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {styles} from './styles';
-import axios from 'axios';
 import {FlatList} from 'react-native-gesture-handler';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-const Catalog = (/* {
-  navigation,
-}: NativeStackScreenProps<RootStackParamList, 'Catalog'> */): JSX.Element => {
-  const [recipes, setRecipes] = useState([]);
+import {useQuery} from 'react-query';
+import {recipes} from '../../utils/recipes';
+import {Loading} from '../../components/loading';
 
-  // Get Foods
-  useEffect(() => {
-    axios
-      .get('http://192.168.100.40:3000/recipes', {
-        headers: {
-          Accept: 'application/json',
-          'content-Type': 'application/json',
-        },
-      })
-      .then((response: any) => {
-        setRecipes(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-        throw new Error(error);
-      });
-  }, []);
+const Catalog = (): JSX.Element => {
+  const {data, isLoading} = useQuery('list-recipes', recipes.get);
 
   const renderItem = ({item}: ListRenderItemInfo<any>) => {
     return (
@@ -56,12 +39,16 @@ const Catalog = (/* {
           <TextInput style={styles.inputText} />
         </View>
       </View>
-      <FlatList
-        keyExtractor={item => item.id}
-        style={styles.flatList}
-        renderItem={renderItem}
-        data={recipes}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          keyExtractor={item => item.id}
+          style={styles.flatList}
+          renderItem={renderItem}
+          data={data}
+        />
+      )}
     </View>
   );
 };
