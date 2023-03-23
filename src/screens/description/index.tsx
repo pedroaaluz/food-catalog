ff93bc192b6d537815db6931869155ebba579a08
pedroaaluz <pedroaluz137@gmail.com>
2023-03-22 22:10:30 -0300

    refactor: :lipstick: add styles file to description file

--- src/screens/description/index.tsx
+++ src/screens/description/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import {styles} from './styles';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import type {StackParamsList} from '../../types/rootStackParamListType';
+import {ScrollView} from 'react-native-gesture-handler';
+import {IngredientCheck} from '../../components/ingredientCheck';
+import {StepRecipe} from '../../components/stepRecipe';
 
 const Description = ({
-  navigation,
-}: NativeStackScreenProps<StackParamsList, 'Description'>): JSX.Element => (
-  <View style={styles.body}>
-    <Text>Favorites</Text>
-    <Button
-      title="Ir para Catalog"
-      onPress={() => navigation.navigate('Catalog')}
-    />
-  </View>
-);
+  route,
+}: NativeStackScreenProps<StackParamsList, 'Description'>): JSX.Element => {
+  const {image, name, ingredients, steps} = route.params;
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <ImageBackground source={{uri: image}} style={{height: 300}}>
+          <View style={styles.imageHeaderContainer}>
+            <Text style={styles.imageHeaderText}>{name}</Text>
+          </View>
+        </ImageBackground>
+      </View>
+      <View style={styles.ingredientsSection}>
+        <Text style={styles.ingredientsHeader}>Ingredients:</Text>
+        <View style={styles.ingredientsList}>
+          {ingredients?.map((ingredient, index) => {
+            return <IngredientCheck ingredient={ingredient} key={index} />;
+          })}
+        </View>
+      </View>
+      <View style={styles.stepsSection}>
+        <Text style={styles.stepsHeader}>Step by Step:</Text>
+        {steps.map((step, index) => {
+          return (
+            <StepRecipe
+              key={index}
+              index={index}
+              description={step.description}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Description;
--- src/screens/description/styles.tsx
+++ src/screens/description/styles.tsx
@@ -1,9 +1,37 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  body: {
+  container: {
+    flex: 1,
+  },
+  imageHeaderContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageHeaderText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 10,
+  },
+  ingredientsSection: {
+    padding: 20,
+    flex: 1,
+  },
+  ingredientsHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginVertical: 10,
+  },
+  ingredientsList: {flex: 1, marginTop: 10},
+  stepsSection: {paddingHorizontal: 20, flex: 1, marginBottom: 40},
+  stepsHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginVertical: 10,
   },
 });
